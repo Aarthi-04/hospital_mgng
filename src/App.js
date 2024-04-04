0268819e7fa31c7be382c64ec32ecc4d1db28f83
Aarthi <raarthi220@gmail.com>
2024-04-04 21:54:29 +0530

    hosp_commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './components/main';
+import Login from './components/login';
+import Bkapp from './components/bkapp';
+import Dashbd from './components/dashbd';
+import Opentry from './components/opentry';
+import Prescription from './components/prescription';
+import Empreg from './components/empreg';
+import Editprof from './components/editprof';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main/>}></Route>
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/bkapp" element={<Bkapp/>}></Route>
+          <Route path="/dashbd" element={<Dashbd/>}></Route>
+          <Route path="/opentry" element={<Opentry/>}></Route>
+          <Route path="/prescription" element={<Prescription/>}></Route>
+          <Route path="/empreg" element={<Empreg/>}></Route>
+          <Route path="/editprof" element={<Editprof/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
